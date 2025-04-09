00136e756551df6b904af944deaa0a12228c8fa1
muka181 <mukamag78@gmail.com>
2025-04-09 13:30:04 +0300

    Add .gitignore and other updates

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>this is an operation application aint it my guy</Text>
+    </View>
+  );
+}
