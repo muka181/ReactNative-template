5b1f2840a23f71d093bb04ba26e8d5a7f7d86e97
muka181 <mukamag78@gmail.com>
2025-04-13 15:52:09 +0300

    react native android template

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import React, { useContext } from 'react';
+import { View, Text, TextInput, Button, ScrollView, StyleSheet, Alert } from 'react-native';
+import { useRouter } from 'expo-router';
+import { DataContext } from '../contexts/DataContext'; // adjust path if needed
 
-export default function Index() {
+export default function HomeScreen() {
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>this is an operation application aint it my guy </Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+    
+
+      <View style={styles.inputGroup}>
+	<Text>edit here</Text>
+       
+      </View>
+
+    
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: '#f3f4f6',
+    flexGrow: 1,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  inputGroup: {
+    marginBottom: 20,
+  }
+});
